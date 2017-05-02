c73a80ea221b3db0b022b54d3780127eb73a13d9
LAPTOP-KDB9RUK3\samru <samruddhi226@gmail.com>
2017-05-02 14:12:12 +0800

    Initial Commit

--- src/main/java/com/websystique/springboot/configuration/JpaConfiguration.java
+++ src/main/java/com/websystique/springboot/configuration/JpaConfiguration.java
@@ -98,7 +98,7 @@ public class JpaConfiguration {
 		Properties properties = new Properties();
 
 		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-		properties.put("hibernate.hbm2ddl.auto", "create-drop");
+		properties.put("hibernate.hbm2ddl.auto", "update");
 		properties.put("hibernate.show_sql", "true");
 		properties.put("hibernate.format_sql", "true");
 		if(StringUtils.isNotEmpty("dummy")){
